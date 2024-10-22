f9f50a86a28d6abc2bb28cab3ea4bcd60419c26a
Ikar <ikarpiatkowski@proton.me>
2024-10-22 18:49:47 +0200

    Refactor tables

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,35 +1,45 @@
-export const Table = ({ openPositions }: { openPositions: any[] }) => (
-  <div class="container mx-auto p-4">
-    <h1 class="text-3xl font-bold mb-4">Open Positions</h1>
-    <table class="table-auto w-full bg-white shadow-md rounded-lg overflow-hidden">
-      <thead class="bg-gray-800 text-white">
-        <tr>
-          <th class="px-4 py-2">Symbol</th>
-          <th class="px-4 py-2">Typ</th>
-          <th class="px-4 py-2">Wolumen</th>
-          <th class="px-4 py-2">Czas otwarcia</th>
-          <th class="px-4 py-2">Cena otwarcia</th>
-          <th class="px-4 py-2">Cena rynkowa</th>
-          <th class="px-4 py-2">Wartość zakupu</th>
-          <th class="px-4 py-2">Zysk brutto</th>
-          <th class="px-4 py-2">Komentarz</th>
-        </tr>
-      </thead>
-      <tbody>
-        {openPositions.map((position, index) => (
-          <tr key={index}>
-            <td class="border px-4 py-2">{position.__EMPTY_1}</td>
-            <td class="border px-4 py-2">{position.__EMPTY_2}</td>
-            <td class="border px-4 py-2">{position.__EMPTY_3}</td>
-            <td class="border px-4 py-2">{position.__EMPTY_4}</td>
-            <td class="border px-4 py-2">{position.__EMPTY_5}</td>
-            <td class="border px-4 py-2">{position.__EMPTY_6}</td>
-            <td class="border px-4 py-2">{position.__EMPTY_7}</td>
-            <td class="border px-4 py-2">{position.__EMPTY_14}</td>
-            <td class="border px-4 py-2">{position.__EMPTY_15}</td>
+export const Table = ({ openPositions }: { openPositions: any[] }) => {
+  if (openPositions.length === 0) return null;
+
+  const excludedHeaders = [
+    "__EMPTY_8", // SL
+    "__EMPTY_9", // TP
+    "__EMPTY_10", // Margin
+    "__EMPTY_11", // Prowizja
+    "__EMPTY_12", // Swap
+    "__EMPTY_13", // Rollover
+    "__EMPTY_15", // Komentarz
+  ];
+
+  const headers = Object.keys(openPositions[0]).filter(
+    (key) => key.startsWith("__EMPTY") && !excludedHeaders.includes(key)
+  );
+
+  return (
+    <div class="container mx-auto p-4">
+      <h1 class="text-3xl font-bold mb-4">Open Positions</h1>
+      <table class="table-auto w-full bg-white shadow-md rounded-lg overflow-hidden">
+        <thead class="bg-gray-800 text-white">
+          <tr>
+            {headers.map((header, index) => (
+              <th key={index} class="px-4 py-2">
+                {openPositions[0][header]}
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
+        </thead>
+        <tbody>
+          {openPositions.slice(1).map((position, index) => (
+            <tr key={index}>
+              {headers.map((header, subIndex) => (
+                <td key={subIndex} class="border px-4 py-2">
+                  {position[header]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,19 +3,10 @@ import { Navbar } from "../components/Navbar.tsx";
 import { StockTable } from "../components/StockTable.tsx";
 import { Table } from "../components/Table.tsx";
 import { DatePickerDemo } from "@/components/DatePicker.tsx";
-import { restClient } from "@polygon.io/client-js";
+// import { restClient } from "@polygon.io/client-js";
 import { Button } from "@/components/Button.tsx";
 import { Input } from "@/components/Input.tsx";
-const rest = restClient(Deno.env.get("POLY_API_KEY"));
-
-rest.stocks
-  .aggregates("AAPL", 1, "day", "2023-01-01", "2023-04-14")
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((e) => {
-    console.error("An error happened:", e);
-  });
+// const rest = restClient(Deno.env.get("POLY_API_KEY"));
 
 export const Home = ({
   stocks,
@@ -40,6 +31,18 @@ export const Home = ({
 
   const sum = parseFloat((lastZyskBrutto + lastZyskBruttoEu * 4.5).toFixed(2));
 
+  // Filter out unnecessary fields and objects
+  const filterOpenPositions = (data: any[]) =>
+    data.filter(
+      (position) =>
+        position.__EMPTY_1 && position.__EMPTY_2 && position.__EMPTY_3
+    );
+
+  const filteredOpenPositions = filterOpenPositions(xlsxData.openPositionsData);
+  const filteredOpenPositionsEu = filterOpenPositions(
+    xlsxDataEu.openPositionsData
+  );
+
   return (
     <html lang="en">
       <head>
@@ -54,16 +57,11 @@ export const Home = ({
           <div class="pt-16 mx-auto flex flex-col items-center justify-center">
             <h1 class="text-4xl font-bold">Welcome to Stock Tracker!</h1>
             <div class="pt-8">
+              <Table openPositions={filteredOpenPositions} />
+              <Table openPositions={filteredOpenPositionsEu} />
+            </div>
+            <div class="pt-8">
               <StockTable stocks={stocks} />
-            </div>
-            <div class="pt-8">
-              <h2 class="text-2xl font-bold">XLSX Data</h2>
-              <pre class="h-80 overflow-y-auto">
-                {JSON.stringify(xlsxData.stocksData, null, 2)}
-              </pre>
-            </div>
-            <div class="pt-8">
-              <Table openPositions={xlsxData.openPositionsData} />
             </div>
             <div class="pt-8">
               <h2 class="text-2xl font-bold">Profits Chart</h2>
